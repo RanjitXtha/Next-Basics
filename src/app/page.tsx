544f884bc4b2bd2e63f3565f4a408e599b6e5205
Ranjit Shrestha <shrestha.ranjit.np@gmail.com>
2026-06-12 10:25:40 +0545

    Update comment for clarity in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function CSRProductsPage() {
   }, []);
 
 //sample commit to test release.io pushes 
-  //test - 26
+  //test - 26 changes
   return (
     <div>
       <h1>CSR Example</h1>
