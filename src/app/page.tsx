68db879573aa0954d4c3a6edc0ce09d95e64bb25
Ranjit Shrestha <shrestha.ranjit.np@gmail.com>
2026-06-12 10:09:42 +0545

    Sendgrid added for email and nodemailer for fallback
    For clients with sendgrid , sendgrid is used for sending messages to client and also provides fallback to use nodemailer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function CSRProductsPage() {
   }, []);
 
 //sample commit to test release.io pushes 
-  //test - 30
+  //test - 31
   return (
     <div>
       <h1>CSR Example</h1>
